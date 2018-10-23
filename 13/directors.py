14902dc27dd13ae654cdb9113058844de3965b71
darkserg89 <python@localhost.localdomain>
2018-10-23 18:02:21 +0300

    finished

--- 13/directors.py
+++ 13/directors.py
@@ -32,25 +32,39 @@ def get_movies_by_director():
 
 def get_average_scores(directors):
     '''Filter directors with < MIN_MOVIES and calculate averge score'''
-    pass
-
-
-def _calc_mean(directors):
-    '''Helper method to calculate mean of list of Movie namedtuples'''
     cnt = Counter()
     for director, movies in directors.items():
         if len(movies) >= 4 :
-            cnt[director] += len(movies)
+            cnt[director] += _calc_mean(movies)
     return cnt
+
+
+def _calc_mean(movies):
+    '''Helper method to calculate mean of list of Movie namedtuples'''
+    number_movies = len(movies)
+    summ = 0
+    for movie in movies:
+        summ+=movie.score
+    result = summ/number_movies
+    return round(result,1)
 
 
 def print_results(directors):
     '''Print directors ordered by highest average rating. For each director
     print his/her movies also ordered by highest rated movie.
     See http://pybit.es/codechallenge13.html for example output'''
-    fmt_director_entry = '{counter}. {director:<52} {avg}'
-    fmt_movie_entry = '{year}] {title:<50} {score}'
+    director_rate=get_average_scores(directors).most_common(20)
+    num = 0
     sep_line = '-' * 60
+    for director,dir_rate in director_rate:
+        num+=1
+        fmt_director_entry = '\n{}. {:<52} {}'.format(num,director,dir_rate)
+        print(fmt_director_entry)
+        print(sep_line)
+        for movie in directors[director]:
+            if movie.year >= MIN_YEAR:
+                fmt_movie_entry = '{year}] {title:<50} {score}'.format(year=movie.year,title=movie.title,score=movie.score)
+                print(fmt_movie_entry)
 
 
 def main():
@@ -65,6 +79,6 @@ if __name__ == '__main__':
 #    main()
     directors = get_movies_by_director()
     pprint(len(directors['Christopher Nolan']))
-    cnt = _calc_mean(directors)
-    pprint(cnt['Christopher Nolan'])
-    pprint(cnt)
+    movies_sergio = directors['Sergio Leone']
+    movies_nolan = directors['Christopher Nolan']
+    print_results(directors)
